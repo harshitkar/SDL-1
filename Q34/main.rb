3683402f75ed99227b2d1fc5b0d307b304d1ee68
harsh <harshitkar104@gmail.com>
2025-04-19 16:38:48 +0530

    fix: Q34

--- Q34/main.rb
+++ Q34/main.rb
@@ -1,19 +1,22 @@
+require 'dotenv'
 require 'mail'
+
+Dotenv.load
 
 Mail.defaults do
   delivery_method :smtp, {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            'onkarj012@gmail.com',      
-    password:             '****************', 
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_APP_PASSWORD'],
     authentication:       :login,
     enable_starttls_auto: true
   }
 end
 
 message = Mail.new do
-  from    'onkarj012@gmail.com'   
-  to      'leoeccentric@gmail.com' 
+  from    ENV['GMAIL_USERNAME']
+  to      ENV['RECIPIENT_EMAIL']
   subject 'Hello from Ruby!'
   body    'This is a test email sent using Ruby + Gmail App Password.'
 end
